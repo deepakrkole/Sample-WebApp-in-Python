e3fbf8721aa317ed110ce94a0462d1234dfa782f
deepakrkole <deepakrkole@gmail.com>
2015-06-01 05:09:24 -0700

    "End points" with HTML pages

--- milestone4.py
+++ milestone4.py
@@ -4,14 +4,15 @@ import os
 
 
 app = Flask(__name__)
-path='C:\Users\DEEPAK\Desktop\Latest_Resumes'
+path='C:\Users\DEEPAK\Desktop\Latest_Resumes'    # Path to the folder whose contents needs to be displayed
+
 def directory():
     message=[]
-    for root, dirl, files in os.walk(path):
+    for root, dirl, files in os.walk(path):     #Loop 1: For Root-Directory with the help of os.walk() method of os module
         dirlist = root
         dirlist=dirlist.split("\\")[-1]
         message.append(dirlist)
-    for fname in files:
+    for fname in files:                         #Loop 2:  For Files under the Root-Directory
         filelist = fname
         filelist=filelist.split("\\")[-1]
         message.append(filelist)
@@ -19,24 +20,24 @@ def directory():
     #return message
     return message
 
-def filesStr(directory):
+def filesStr(directory):                        #Function 2: function for displaying sub directory contents through parameter passing
     directoryStr=[]
-    for root, dirl, files in os.walk(directory):
+    for root, dirl, files in os.walk(directory): #Loop 1: For NonRoot-Directory with the help of os.walk() method of os module
         dirlist = root
         dirlist=dirlist.split("\\")[-1]
         directoryStr.append(dirlist)
-    for fname in files:
+    for fname in files:                           #Loop 2:  For Files under the NonRoot-Sub-Directory, if exists
         filelist = fname
         filelist=filelist.split("\\")[-1]
         directoryStr.append(filelist)
     return directoryStr
 
-@app.route('/')
+@app.route('/')                                 # end Point for inital page load, which will generate Get reguest
 def hello_world():
-    return render_template('directory.html', directories = directory())
+    return render_template('directory.html', directories = directory())  # Send directory structure with html name
 
 
-@app.route('/<directory>')
+@app.route('/<directory>')          # end Point for directoris, sub-directories through end-point
 def hello(directory):
     print directory
     #direct=request.args.
