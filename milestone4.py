a94753eba01b42e554b2bf6b59888f5fd1f946fa
deepakrkole <deepakrkole@gmail.com>
2015-06-01 04:33:53 -0700

    "End points"

--- milestone4.py
+++ milestone4.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import render_template
+import os
 
+
+app = Flask(__name__)
+path='C:\Users\DEEPAK\Desktop\Latest_Resumes'
 def directory():
     message=[]
     for root, dirl, files in os.walk(path):
@@ -15,11 +19,29 @@ def directory():
     #return message
     return message
 
-app = Flask(__name__)
+def filesStr(directory):
+    directoryStr=[]
+    for root, dirl, files in os.walk(directory):
+        dirlist = root
+        dirlist=dirlist.split("\\")[-1]
+        directoryStr.append(dirlist)
+    for fname in files:
+        filelist = fname
+        filelist=filelist.split("\\")[-1]
+        directoryStr.append(filelist)
+    return directoryStr
 
 @app.route('/')
 def hello_world():
     return render_template('directory.html', directories = directory())
+
+
+@app.route('/<directory>')
+def hello(directory):
+    print directory
+    #direct=request.args.
+    directory=path+'\\'+directory
+    return render_template('Files.html', directories = filesStr(directory))
 
 if __name__ == '__main__':
     app.run()
